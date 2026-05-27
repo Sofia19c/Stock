78bbcf09d9b446abcf32fd70252590b6bac50091
sofia19c <sofivampaireknight@gmail.com>
2026-05-26 19:39:53 -0500

    Primer commit inventario

---
+++ Public/script.js
@@ -0,0 +1,212 @@
+//AQUI IRA LA LOGICA DEL BOTON
+const boton = document.getElementById("btn-agregar");
+
+//async dice que habran tareas que toman tiempo
+boton.addEventListener("click", async () => {
+
+    //id para conectar cada casilla en html con java script
+    const nombre = document.getElementById("nombre").value;
+    const cantidad = document.getElementById("cantidad").value;
+    const precio = document.getElementById("precio").value;
+
+    //Aqui no permite que se guarden valores vacios, se deben completar las 3 celdas
+    if(!nombre || !cantidad ||! precio){
+        alert("completa todos los campos");
+        return;
+    }
+     //objeto en java 
+    const producto = {
+        nombre,
+        cantidad,
+        precio
+    };
+
+    //fetch es la herramienta nativa de JavaScript para hacer peticiones por internet. 
+    //Aquí está tocando la puerta de una dirección en el servidor llamado /productos.
+    //await le dice a JavaScript: "Detente un momento en esta línea y espera a que el 
+    // servidor responda antes de continuar con la siguiente"
+    const respuesta = await fetch("/productos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(producto)
+    });
+
+    //aqui se recibe la respuesta del servidor y se transforma en n objeto js facil de leer
+    const data = await respuesta.json();
+
+    // lansa mensaje de alerta avisando que la operacion fue exitosa
+    alert(data.mensaje);
+
+    //recargar automaticamente despues de agregar productos
+    cargarProductos();
+
+    //limpiar inputs
+    // -- SIGNIFICADO INPUTS --
+    //todos aquellos datos, recursos, materiales o información que se ingresan a un sistema o proceso 
+    // para que este pueda funcionar, transformarlos y generar un resultado (conocido como output o salida).
+    document.getElementById("nombre").value = "";
+    document.getElementById("cantidad").value = "";
+    document.getElementById("precio").value = "";
+});
+
+//FUNCION PARA MOSTRAR LOS PRODUCTOS
+async function cargarProductos() {
+
+    //fetch("/productos"): Envía una petición al servidor para pedir los datos
+    //que están en la ruta /productos.
+    const respuesta = await fetch("/productos");
+    
+    //Guarda la lista de productos real (un array) que ya puedo recorrer
+    const productos = await respuesta.json();
+
+    const textoBusqueda = document.getElementById("busqueda")
+    ?.value
+    //Toma el texto que escribió el usuario y lo convierte por completo a minúsculas.
+    .toLowerCase() || "";
+
+    // Aquí el código viaja al documento HTML de la página web y busca un elemento (una etiqueta <table>)
+    //que tenga el atributo id="tabla-productos" y lo guarda en la variable tabla para poder modificarlo.
+    const tabla = document.getElementById("tabla-productos");
+
+    //Aqui cuenta cuantos productos diferentes hay registrados en el sistema
+    const totalProductos =
+        productos.length;
+
+    //crea una nuev lista con los productos que cumplan la condicion
+    //en este caso, los productos que sean menores a 5 
+    const stockBajo =
+        productos.filter(
+            producto => Number(producto.cantidad) <5
+        ).length;
+
+    //Aqui se calcula el valor final del inventario
+    const valorInventario =
+        //reduce toma una lista de muchas cosas y la reduce a un solo valor final (en este caso, la suma total de dinero)
+        productos.reduce(
+            (total, producto) =>
+                total + (
+                    Number(producto.precio) *
+                    Number(producto.cantidad)
+                ),
+            //Aqui le digo a js que empiece a contar desde 0
+            0
+        );
+
+    //Aqui se actualiza el total de productos 
+    document.getElementById(
+        "total-productos"
+    ).textContent = totalProductos;
+
+    //Funciona igual que la linea anterior 
+    //Actualiza la alerta de poco Stock
+    document.getElementById(
+        "stock-bajo-total"
+    ).textContent = stockBajo;
+
+    //Actualiza el valor del inventario con un formato mas bonito
+    document.getElementById(
+        "valor-total"
+    ).textContent =  `$${valorInventario.toLocaleString()}`;
+
+    // Esta línea limpia la tabla. Borra cualquier fila o texto que hubiera tenido antes. Esto se hace para que, si vuelves a cargar los productos,
+    // no se dupliquen ni se acumulen con los viejos.
+    tabla.innerHTML = "";
+
+    productos
+    .filter(producto =>
+        producto.nombre
+            .toLowerCase()
+            .includes(textoBusqueda)
+    )
+
+    //Como productos es una lista, usamos .forEach() para recorrerlos uno por uno. En cada vuelta del ciclo, 
+    //el producto actual se guardará en la variable producto.
+    .forEach(producto => {
+        const estado =
+            Number(producto.cantidad) < 5
+                //si la cantidad del producto es menor a 5 
+                ? "⚠ Poco stock"
+                //si es verdadero lo guarda en la variable estado con el texto Poco stock
+                : "✅ Disponible";
+
+        //el operador += significa "súmale esto a lo que ya había". En cada vuelta del ciclo,
+        //agrega una nueva fila (<tr>)al final de la tabla 
+        //Las comillas invertidas (``) permiten crear un template string, lo que nos deja mezclar texto HTML
+        //con variables de JavaScript usando la sintaxis ${variable}
+        //los <td> etc son celdas de la tabla 
+        tabla.innerHTML += `
+            <tr>
+                <td>${producto.nombre}</td>
+                <td>${producto.cantidad}</td>
+                <td>$${producto.precio}</td>
+               <td class="${
+                    Number(producto.cantidad) < 5
+                        ? "stock-bajo"
+                        : "stock-ok"
+                }">
+                ${estado}
+                </td>
+
+                <td>
+                    <button onclick="editarCantidad(${producto.id}, 1)">
+                        +
+                    </button>
+
+                    <button onclick="editarCantidad(${producto.id}, -1)">
+                        -
+                    </button>
+
+                    <button onclick="eliminarProducto(${producto.id})">
+                        🗑️
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+}
+//Recibe dos parámetros: el id del producto a modificar y el cambio (que será 1 o -1).
+    async function editarCantidad(id, cambio) {
+       
+        //productos/${id}`: Usando comillas invertidas, le insertamos el ID directamente a la URL.
+        //Si toque el producto 12, la petición se enviará a /productos/12`
+        await fetch(`/productos/${id}`, {
+            //usamos PUT porque nuestra intención es actualizar o modificar un dato existente en el servidor
+            method: "PUT",
+            //contiene la etiqueta del tipo de paquete que se envia
+            headers:{
+                "Content-Type": "application/json"
+            },
+
+            //
+            body: JSON.stringify({
+                cambio
+            })
+        });
+
+        cargarProductos();
+    }  
+    
+    async function eliminarProducto(id) {
+        
+        const confirmar = confirm(
+            "¿Seguro que quieres eliminar este producto?"
+        );
+
+        if (!confirmar) return;
+
+        await fetch(`/productos/${id}`, {
+            method: "Delete"
+        });
+
+        cargarProductos();
+    }
+cargarProductos();
+
+//Actualizar al escribir en la barra de busqueda
+document
+.getElementById("busqueda")
+.addEventListener("input", () =>{
+    cargarProductos();
+});
---
+++ server.js
@@ -0,0 +1,225 @@
+//Importa la librería Express, que sirve para crear servidores web y APIs
+const { error } = require("console");
+const express = require("express");
+// Importa el modulo fs(File System)
+// Se usa para leer, escribir o manipular archivos en el sistema
+const fs = require("fs");
+//Importa el módulo nativo path.
+//Ayuda a trabajar con rutas de archivos y carpetas.
+const path = require ("path");
+const { json } = require("stream/consumers");
+//Crea una aplicación de Express.
+//app será el objeto principal para configurar rutas, middleware y el servidor.
+const app = express();
+// puerto donde correra el servidor
+const PORT = process.env.PORT || 3000;
+//Activa un middleware para que Express pueda entender datos en formato JSON.
+//Muy útil cuando el frontend envía información mediante POST, PUT, etc.
+app.use(express.json());
+//Indica que la carpeta public contendrá archivos estáticos.
+app.use(express.static("public"));
+
+//MOSTRAR PRODUCTOS EN LA TABLA
+//Aqui esta escuchando la peticion POST en la direccion /producto
+//esta peticion fue enviada en la linea 23 del archivo script.js
+//req contiene todo lo que viene de afuera
+//res es lo que el servidor usara para contestarle al navegador
+app.post("/productos",(req, res)=>{
+    //Aqui es donde llega el producto que el usuario escribio (nombre, cantidad, precio)
+    //el servidor lo extrae del cuerpo de la peticion y lo guarda en la variable
+    const nuevoProducto = req.body;
+    //busca donde guardar esa variable, y dirname es en el directorio actual, en data en produc..json
+    const rutaArchivo = path.join(__dirname, "data", "productos.json");
+    //lee el archio con los productos ingresados , utf 8 Es el formato de codificación para que 
+    // Node lea el archivo como texto con acentos y caracteres normales, y no como datos binarios
+    //(error, data) es una función que se ejecutará cuando termine de leer. Si algo sale mal,
+    //se llena la variable error. Si todo sale bien, el contenido del archivo se guarda en data.
+    fs.readFile(rutaArchivo,"utf8", (error,data)=>{
+
+        if (error){
+            return res.status(500).json({
+                mensaje:"Error leyendo archivo"
+            });
+        }
+
+        //JSON.parse(data): El archivo se lee como texto plano. 
+        //esta línea lo transforma mágicamente en una lista de JavaScript (un array) para poder manipularlo.
+        const productos = JSON.parse(data);
+        //Para que los productos no se confundan, les creao un id único. Date.now()
+        nuevoProducto.id = Date.now();
+        //Meto el nuevo producto al final de la lista que acabo de leer.
+        productos.push(nuevoProducto);
+
+        //guarda la lista actualizada
+        fs.writeFile(
+            rutaArchivo,
+            //convierte la lista de javascript en un archivo de texto plano para que sea legible
+            JSON.stringify(productos,null,2),
+            (error) =>{
+                if (error){
+                    return res.status(500).json({
+                        mensaje: "Error guardando producto"
+                    });
+                }
+
+                res.json({
+                    mensaje: "Producto guardado correctamente"
+                });
+            }
+        );
+    });
+});
+
+
+//escucha las peticiones de tipo GEST en la direccion /productos
+app.get("/productos",(req, res) => {
+    //construir la direccion donde estan guardado el archivo con los productos
+    const rutaArchivo =path.join(
+        __dirname,
+        "data",
+        "productos.json"
+    );
+    //Aqui abre y lee el contenido del archivo de texto
+    fs.readFile(rutaArchivo, "utf8", (error, data) =>{
+        if(error){
+            return res.status(500).json({
+                mensaje: "Error leyendo productos"
+            });
+        }
+        //Transformar el texto plano del archivo en datos reales de JavaScript.
+        const productos = JSON.parse(data);
+
+        //: Enviar los productos de vuelta al navegador que los pidió y cerrar la función.
+        res.json(productos);
+    });
+});
+
+//El método HTTP PUT se usa por convención cuando queremos actualizar o modificar datos que ya existen.
+app.put("/productos/:id",(req, res)=>{
+    // Extrae el ID que venia en la URL y lo pone como numero real
+    const id =Number(req.params.id);
+    //extrae la peticion el 1 o -1 que agregue
+    const cambio = req.body.cambio;
+
+    //construyo la direccion donde esta guardado el archivo de texto productos.json
+    const rutaArchivo = path.join(
+         __dirname,
+        "data",
+        "productos.json"
+    );
+
+    //
+    fs.readFile(rutaArchivo, "utf8", (error, data) =>{
+        if(error){
+            return res.status(500).json({
+                mensaje: "Error leyendo archivo"
+            });
+        }
+
+        //Como el archivo se lee como texto plano
+        //JSON.parse lo transforma mágicamente en un array de objetos de JavaScript
+        const productos = JSON.parse(data);
+
+        //Usa la función .find() para buscar dentro de la lista el primer producto 
+        //cuyo id coincida exactamente con el id que venía en la URL.
+        const producto = productos.find(
+            p => p.id ===id
+        );
+
+        if(!producto){
+            return res.status(500).json({
+                mensaje:"Producto no encontrado"
+            }); 
+        }
+
+        //Toma la cantidad actual del producto encontrado, 
+        //se asegura de que sea un número, y le suma el cambio (que puede ser +1 o -1)
+        //ademas muestra un error si el producto es menor a 0 (para evitar valores negativos)
+        const nuevaCantidad =
+            Number(producto.cantidad) + cambio; 
+
+        if (nuevaCantidad < 0){
+            return res.status(400).json({
+                mensaje: "La cantidad no puede ser menor a 0"
+            });
+        }
+
+        producto.cantidad = nuevaCantidad;
+        
+        //Como el cambio solo se hizo en la memoria temporal del servidor,
+        //ahora hay que sobreescribir el archivo físico para guardar los cambios permanentemente.
+        fs.writeFile(
+            rutaArchivo,
+            //transforma nuestro array de JavaScript de vuelta a un texto JSON limpio y ordenado
+            JSON.stringify(productos, null, 2),
+            (error) => {
+
+                if (error){
+                    return res.status(500).json({
+                    mensaje: "Error actualizado"
+                    });
+            }
+              
+                res.json({
+                    mensaje: "Cantidad actualizada"
+                });
+            }
+        );
+    });
+});
+
+app.delete("/productos/:id", (req, res) =>{
+
+    const id= Number(req.params.id);
+
+    const rutaArchivo= path.join(
+        __dirname,
+        "data",
+        "productos.json"
+    );
+
+    fs.readFile(rutaArchivo, "utf-8", (error, data) =>{
+
+        if (error){
+            return res.status(500).json({
+                mensaje: "Error leyendo el archivo"
+            });
+        }
+
+        //Sirve para convertir una cadena de texto (string) en formato JSON
+        let productos = JSON.parse(data);
+
+        //eliminar producto de la lista
+        // => significa que examina cada producto de la lista 
+        //producto.id !== id: Esta es la condición. 
+        //Compara el ID del producto actual con el id que quiero eliminar
+        productos = productos.filter(
+            producto => producto.id !== id
+        );
+
+
+        //Aqui se guardam los cambios en un archivo de texto
+        fs.writeFile(
+            rutaArchivo,
+            JSON.stringify(productos, null, 2),
+            (error) => {
+
+                if (error){
+                    return res.status(500).json({
+                        mensaje: "Error liminando"
+                    });
+                }
+
+                res.json({
+                    mensaje: "Producto eliminado"
+                });
+            }
+        );
+
+    });
+});
+
+
+app.listen(PORT, () => { //escucha en el puerto
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
